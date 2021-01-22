1bdf934ab67cf2df6dc8f42468d7d5ad4ecf4656
Alice Yang <yangalice212@gmail.com>
2021-01-23 00:34:46 +0800

    ver.3

--- js/all.js
+++ js/all.js
@@ -1,20 +1,28 @@
 var calBtn = document.querySelector('.calculate');
 var bmiList = document.querySelector('.bmiList');
-var bmiData = JSON.parse(localStorage.getItem('listData')) || [];
+var bmiData = JSON.parse(localStorage.getItem('bmiData')) || [];
 var height = document.querySelector('.inputHeight');
 var weight = document.querySelector('.inputWeight');
 var reset = document.querySelector('.reset');
-var clearAllBtn = document.querySelector('.clearAllBtn')
+var clearAllBtn = document.querySelector('.clearAllBtn');
+var bmiItem = {};
 
 var today = new Date();
-var todayDate = (today.getMonth()+1)+'-'+today.getDate()+'-'+today.getFullYear()+'';
+var todayDate = (today.getMonth()+1)+'-'+today.getDate()+'-'+today.getFullYear();
 
+calBtn.addEventListener('click',bmiCalculate,false);
+bmiList.addEventListener('click',delItem,false);
+reset.addEventListener('click',resetData,false);
+clearAllBtn.addEventListener('click',clearAllData,false);
+updateList(bmiData);
+
+//更新 BMI 紀錄
 function updateList(items){
     var str = '';
     for(var i=0;i<items.length;i++){
         str +=      
         `
-        <li class="${bmiData[i].status}">
+        <li style="border-left: 7px solid ${bmiItem.color}">
             <h4 class="itemText">${bmiData[i].statusText}</h4>
             <h4>BMI</h4>
             <p>${bmiData[i].bmi}</p>
@@ -30,59 +38,65 @@ function updateList(items){
     //將動態新增的 HTML 標籤加入 bmiList 中
     bmiList.innerHTML = str;
 }
-
+//計算 BMI
 function bmiCalculate(e){
     e.preventDefault();
-    var bmiItem = {};
     BMI(bmiItem);
     bmiData.push(bmiItem);
     localStorage.setItem('bmiData',JSON.stringify(bmiData));
-
+    console.log(bmiItem.color);
     //更新
     updateList(bmiData);
-    
+    clearBtnToggle();
+
     document.querySelector('.status h3').textContent = bmiItem.bmi;
     document.querySelector('.statusText').textContent = bmiItem.statusText;
 
     let status = document.querySelector('.status');
-    let statusInner = document.querySelector('.status-inner')
+    let statusInner = document.querySelector('.status-inner');
     calBtn.style.display = 'none';
     status.style.display = 'flex';
-    //status.setAttribute('class',"status " + `${bmiItem.status}`);
-    statusInner.setAttribute('class', "status-inner " + `${bmiItem.status}`);
 
-    clearBtnToggle();
+    statusInner.setAttribute('class', "status-inner " + `${bmiItem.status}`);
+    statusInner.setAttribute('style', "color:" + `${bmiItem.color}` + ";border: 6px solid " + `${bmiItem.color}`);
+    document.querySelector('.status-inner a').setAttribute('style', "background:" + `${bmiItem.color}`);
+    document.querySelector('.statusText').setAttribute('style', "color:" + `${bmiItem.color}`);
 }
 //BMI
 function BMI(item){
     let Height = parseInt(height.value);
     let Weight = parseInt(weight.value);
     let bmi = (Weight / Math.pow(Height/100, 2)).toFixed(2); //小數點後兩位
-
     switch(true){
         case bmi <= 18.5:
             item.status = "light";
             item.statusText = "過輕";
+            item.color = "#31BAF9";
             break;
         case bmi > 18.5 && bmi<=25:
             item.status = "balance";
             item.statusText = "理想";
+            item.color = "#86D73F";
             break;
         case bmi > 25 && bmi<=30:
             item.status = "fat";
             item.statusText = "過重";
+            item.color = "#FF982D";
             break;
         case bmi > 30 && bmi<=35:
             item.status = "fat-1";
             item.statusText = "輕度肥胖";
+            item.color = "#FF6C03";
             break;
         case bmi > 35 && bmi<=40:
             item.status = "fat-2";
             bmiItem.statusText = "中度肥胖";
+            item.color = " #FF6C03";
             break;
         case bmi > 40:
             item.status = "fat-3";
             item.statusText = "重度肥胖";
+            item.color = "#FF1200";
             break;
         default:
             alert('請輸入身高體重');
@@ -120,7 +134,6 @@ function clearAllData(e){
     e.preventDefault();
     bmiData.splice(0,99);
     localStorage.removeItem('bmiData');
-
     updateList(bmiData);
     this.style.display = 'none';
 }
@@ -135,8 +148,3 @@ function clearBtnToggle(){
     }
 }
 
-updateList(bmiData);
-calBtn.addEventListener('click',bmiCalculate,false);
-bmiList.addEventListener('click',delItem,false);
-reset.addEventListener('click',resetData,false);
-clearAllBtn.addEventListener('click',clearAllData,false);
